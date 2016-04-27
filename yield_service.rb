941efc9f00dc36705510f4d470fce43f42a60053
Ingrid Anzola <ianzola@hotmail.com>
2016-04-27 18:23:24 +1000

    Connecting the cron class to the view. Now you can get the service working through the URL

--- yield_service.rb
+++ yield_service.rb
@@ -1,12 +1,10 @@
 require 'sinatra'
+require './src/cron'
 
 class YieldService < Sinatra::Base
-  get '/' do
-    "Hello, world!"
+  get '/:date' do
+    cron = Cron.new
+    @next_event = cron.next_event(Date.parse(params[:date]))
     erb :index
-  end
-
-  get '/:name' do
-    "Hello, #{params[:name]}!"
   end
 end
